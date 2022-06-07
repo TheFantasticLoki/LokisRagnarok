6514440e3dffad269ef2ed77a1f4016922933df6
Austin Steyer <asteyer11@gmail.com>
2022-06-06 21:33:33 -0600

    Updated Install Method and Added Uninstaller.

--- install_LRZ.bat
+++ install_LRZ.bat
@@ -1,12 +1,18 @@
 @echo off
 
-xcopy /i "%CD%\privategamelobby_project.lua" "%localappdata%\Plutonium\storage\t6\ui_mp\t6\menus" /y 
+xcopy /i "%CD%\" "%localappdata%\Plutonium\storage\t6\mods\Lokis-Ragnarok" /E /y
+
+xcopy /i "%CD%\menus\privategamelobby_project.lua" "%localappdata%\Plutonium\storage\t6\ui_mp\t6\menus" /y 
 
 xcopy /i "%CD%\scripts" "%localappdata%\Plutonium\storage\t6\scripts" /E /y
 
 xcopy /i "%CD%\images" "%localappdata%\Plutonium\storage\t6\images" /E /y
 
-xcopy /i "%CD%\Plutonium No Update.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs" /y 
+xcopy /i "%CD%\shortcuts\Plutonium No Update.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs" /y 
+
+xcopy /i "%CD%\shortcuts\Install LRZ++.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs" /y 
+
+xcopy /i "%CD%\shortcuts\Uninstall LRZ++.lnk" "%appdata%\Microsoft\Windows\Start Menu\Programs" /y 
 
 @echo Loki's Ragnarok ++ Install Completed!
 
---
+++ uninstall_LRZ.bat
@@ -0,0 +1,31 @@
+@echo off 
+
+del "%localappdata%\Plutonium\storage\t6\ui_mp\t6\menus\privategamelobby_project.lua"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\Loki-RagnarokV1.5.1.gsc"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\Loki-RagnarokV1.5.1.gsc.bak"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\zm_transit\lab.gsc"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\zm_transit\forest.gsc"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\zm_transit\nacht der untoten.gsc"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\zm_transit\the bus ride.gsc"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\zm_transit\the diner.gsc"
+
+del "%localappdata%\Plutonium\storage\t6\scripts\zm\zm_prison\rooftop_and_bridge.gsc"
+
+del "%localappdata%\Plutonium\storage\t6\images\camo_diamond_alt_nml.iwi"
+
+del "%localappdata%\Plutonium\storage\t6\images\camo_diamond_nml.iwi"
+
+del "%localappdata%\Plutonium\storage\t6\images\camo_gold_alt_nml.iwi"
+
+del "%localappdata%\Plutonium\storage\t6\images\camo_gold_nml.iwi"
+
+del "%localappdata%\Plutonium\storage\t6\images\camo_zombies_nml.iwi"
+
+del "%localappdata%\Microsoft\Windows\Start Menu\Programs\Plutonium No Update.lnk"
